b06daba2584785de9deacb8aeb81bec6877b18e0
eb3831 <eb3831@bs.amalnet.k12.il>
2026-03-26 16:10:29 +0200

    removed master activity layout and updated bottom navigation bar height in teacher and student main activities

--- app/src/main/java/com/example/learn2drive/Activities/MasterActivity.java
+++ app/src/main/java/com/example/learn2drive/Activities/MasterActivity.java
@@ -24,7 +24,6 @@ public class MasterActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_master);
 
         initViews();
     }
